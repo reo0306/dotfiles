1559154ae9609524a4e7e1fc181eadbc663810ca
reo0306 <reo19840306@gmail.com>
2024-06-27 00:05:46 +0900

    add nvim

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,29 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local lualine = require("lualine")
+
+    local colors = {
+      bg = "#202328",
+      fg = "#bbc2cf",
+      yellow = "#ECBE7B",
+      cyan = "#008080",
+      darkblue = "#081633",
+      green = "#98be65",
+      orange = "#FF8800",
+      violet = "#a9a1e1",
+      magenta = "#c678dd",
+      blue = "#51afef",
+      red = "#ec5f67",
+    }
+
+    local config = {
+      options = {
+        theme = 'onedark',
+      },
+    }
+
+  lualine.setup(config)
+  end,
+}
